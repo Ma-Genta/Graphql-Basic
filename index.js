8924462cd1ca5c1a7bb568ff85093935962966ee
Carlos Brice <aurelio.gomez1999@gmail.com>
2020-05-30 17:58:56 -0500

    Antes de argumentos

--- index.js
+++ index.js
@@ -1,29 +1,22 @@
 'use strict'
 const express = require('express');
-const {graphql, buildSchema} = require('graphql');
-const gqlMiddleware = require('express-graphql');
+const { buildSchema } = require('graphql');
+const gqlMiddleware = require('express-graphql'); //Middleware de graphql, para usarse en express
+const { readFileSync } = require('fs');
+const { join } = require('path');
+const resolvers = require('./lib/resolvers');
 const app = express();
 const PORT = process.env.PORT || 3000;
+
 //Definicion del esquema
+const schema = buildSchema(readFileSync(join(__dirname,'lib', 'schema.gql'), 'utf-8')); //Importacion del Schema
 
-const schema = buildSchema(`
-    type Query {
-        hello: String,
-        saludo: String
-    }
-`);
+app.use('/api', gqlMiddleware({
+    schema: schema, //Invocamos Schema 
+    rootValue: resolvers, //Invocamos metodos 
+    graphiql: true
+}));
 
-//Config resolvers
 
-const resolvers = {
-    hello: () => 'Hola mundo',
-    saludo: () => 'Holaaaa'
-};
-
-//Ejecucion del Query
-
-graphql(schema, `{hello, saludo}`, resolvers).then(result => {
-    console.log(result);
-}).catch((err) => {
-    console.error(err);
-});
+app.listen(PORT, console.log(`Escuchando en el puerto ${PORT}.
+http://localhost:${PORT}/api`));
